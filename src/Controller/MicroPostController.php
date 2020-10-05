80d886ce12d27293a1c7cf64e97399fd0ff05dd8
Grabatui <anatoliy.bibanaev@gmail.com>
2020-10-05 21:09:20 +0500

    25. Micro posts index template and controller for it

---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MicroPostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/micro-post")
+ */
+class MicroPostController extends AbstractController
+{
+    /**
+     * @var MicroPostRepository
+     */
+    private $microPostRepository;
+
+    public function __construct(MicroPostRepository $microPostRepository)
+    {
+        $this->microPostRepository = $microPostRepository;
+    }
+
+    /**
+     * @Route("/", name="micro_post_index")
+     */
+    public function index()
+    {
+        $posts = $this->microPostRepository->findAll();
+
+        return $this->render('micro-post/index.html.twig', compact('posts'));
+    }
+}
