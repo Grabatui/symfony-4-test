d34f3a95bcbba87ad42b329eb4b4e6b58e6e77d8
Grabatui <anatoliy.bibanaev@gmail.com>
2020-10-24 19:30:32 +0500

    63. Likes controller

---
+++ src/Controller/LikeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/likes")
+ */
+class LikeController extends AbstractController
+{
+    /**
+     * @Route("/like/{id}", name="likes_like")
+     *
+     * @param MicroPost $microPost
+     * @return JsonResponse
+     */
+    public function like(MicroPost $microPost)
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if (!($currentUser instanceof User)) {
+            return $this->json([], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $microPost->like($currentUser);
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json(['count' => $microPost->getLikedBy()->count()]);
+    }
+
+    /**
+     * @Route("/unlike/{id}", name="likes_unlike")
+     *
+     * @param MicroPost $microPost
+     * @return JsonResponse
+     */
+    public function unlike(MicroPost $microPost)
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if (!($currentUser instanceof User)) {
+            return $this->json([], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $microPost->unlike($currentUser);
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json(['count' => $microPost->getLikedBy()->count()]);
+    }
+}
